05f2e684f038dcce5b17689a4b4ad9e5f17325e0
[junho] <akcnl5216@gmail.com>
2024-01-31 18:01:01 +0900

    create_ec2_on_trraform

--- ec2.tf
+++ ec2.tf
@@ -1,9 +1,3 @@
-resource "aws_key_pair" "sjh_key" {
- key_name = "sjh_key"
- public_key = "ssh -rsa ca:a8:fe:62:d3:12:e9:f9:4c:42:49:83:ab:8f:12:a5:fd:64:90:d3"
-}
-
-
 resource "aws_instance" "pub_instance1" {
  ami = "ami-0f3a440bbcff3d043"
  instance_type = "t2.micro"
