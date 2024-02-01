f0bf82d0ead0f18642a6070fe94992a215cee896
[junho] <akcnl5216@gmail.com>
2024-02-01 12:17:34 +0900

    create_ec2_on_terraform

--- ec2.tf
+++ ec2.tf
@@ -4,6 +4,7 @@ resource "aws_instance" "pub_instance1" {
  key_name = "sjh_key"
  subnet_id = aws_subnet.PUB-test_subnet.id
  vpc_security_group_ids = [aws_security_group.pub_sg.id]
+ associate_public_ip_address = true
  
  tags = {
   Name = "sjh_test"
