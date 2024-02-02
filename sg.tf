402425b9be9e642ff9344e62d3e78c219cb40cbf
[junho] <akcnl5216@gmail.com>
2024-02-02 13:40:27 +0900

    Security_group

---
+++ sg.tf
@@ -0,0 +1,28 @@
+resource "aws_security_group" "pub_sg" {
+ vpc_id = aws_vpc.sjh_testvpc.id
+ name = "pub_SG"
+ description = "pub_SG"
+ tags = {
+  Name = "sjh_pub_SG"
+ }
+ ingress {
+  from_port = 22
+  to_port = 22
+  protocol = "TCP"
+  cidr_blocks = ["211.115.223.215/32"]
+  }
+ ingress {
+  from_port = 80
+  to_port = 80
+  protocol = "TCP"
+  cidr_blocks = ["0.0.0.0/0"]
+  }
+ egress {
+  from_port = 0
+  to_port = 0
+  protocol = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
