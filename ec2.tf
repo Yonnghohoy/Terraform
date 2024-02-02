d757bb66a8424baa7f549b62c0cc915c056b80bc
[junho] <akcnl5216@gmail.com>
2024-02-02 14:39:20 +0900

    helloworld_pub

--- ec2.tf
+++ ec2.tf
@@ -1,13 +1,21 @@
-resource "aws_instance" "pub_instance1" {
- ami = "ami-0f3a440bbcff3d043"
- instance_type = "t2.micro"
- key_name = "sjh_key"
- subnet_id = aws_subnet.PUB-test_subnet.id
- vpc_security_group_ids = [aws_security_group.pub_sg.id]
- associate_public_ip_address = true
- 
- tags = {
-  Name = "sjh_test"
- }
+resource "aws_instance" "test_instance" {
+  ami           = "ami-0f3a440bbcff3d043"
+  instance_type = "t2.micro"
+  key_name      = "sjh_key"
+  subnet_id     = aws_subnet.PUB-test_subnet.id
+  vpc_security_group_ids = [aws_security_group.pub_sg.id]
+  associate_public_ip_address = true
+  user_data = <<-EOF
+  #!/bin/bash
+  apt-get install -y apache2
+  systemctl enable apache2
+  apt update
+  apt upgrade -y
+  echo "hello world" > /var/www/html/index.html
+  EOF
+
+  tags = {
+    Name = "sjh_test_instance"
+  }
 }
 
