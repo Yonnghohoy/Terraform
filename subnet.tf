6c8ac1cf61cfc4bea39824660d7a7170fcca961e
[junho] <akcnl5216@gmail.com>
2024-02-05 17:24:10 +0900

    make_ec2_on_terraform

--- subnet.tf
+++ subnet.tf
@@ -24,3 +24,19 @@ resource "aws_subnet" "PRI2-test_subnet" {
   Name = "sjh_test_PRI2"
   }
 }
+
+resource "aws_route_table_association" "pub_rtb_association1" {
+  subnet_id = aws_subnet.PUB-test_subnet.id
+  route_table_id = aws_route_table.sjh_test_pub_rtb.id
+  }
+
+
+resource "aws_route_table_association" "pri_rtb_association1" {
+  subnet_id = aws_subnet.PRI1-test_subnet.id
+  route_table_id = aws_route_table.sjh_test_pri_rtb.id
+  }
+
+resource "aws_route_table_association" "pri_rtb_association2" {
+  subnet_id = aws_subnet.PRI2-test_subnet.id
+  route_table_id = aws_route_table.sjh_test_pri_rtb.id
+  }
