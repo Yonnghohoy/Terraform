a885b1f7a17cc18efff82813109ef5ae02f86453
[junho] <akcnl5216@gmail.com>
2024-02-05 15:33:39 +0900

    pri_nat

---
+++ nat.tf
@@ -0,0 +1,15 @@
+resource "aws_eip" "eip_1" {
+  vpc = true
+  lifecycle {
+   create_before_destroy = treu
+  }
+
+
+resource "aws_nat_gateway" "nat_gateway_1"
+  allocation_id = aws_eip.eip_1.id
+  subnet_id = aws_subnet.PUB-test_subnet.id
+  tags = {
+   Name = "sjh-nat-gateway-1"
+  }
+}
+
