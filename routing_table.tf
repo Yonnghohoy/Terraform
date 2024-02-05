152a8cdfca364b1bb97cfcfee5fa99d88549b4fb
[junho] <akcnl5216@gmail.com>
2024-02-05 16:59:45 +0900

    nat_test

--- routing_table.tf
+++ routing_table.tf
@@ -11,10 +11,10 @@ resource "aws_route_table" "sjh_test_pub_rtb" {
 
 resource "aws_route_table" "sjh_test_pri_rtb" {
  vpc_id = aws_vpc.sjh_testvpc.id
- route {
-  cidr_block = "0.0.0.0/0"
-  gateway_id = aws_nat_gateway.nat_gateway_1.id
-  }
+# route {
+#  cidr_block = "0.0.0.0/0"
+#  gateway_id = aws_nat_gateway.nat_gateway_1.id
+#  }
  
  tags = {
   Name = "sjh_test_pri_rtb"
