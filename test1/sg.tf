b61ab672ce3d331e7b1ea1b92f0a05922bafadbf
[junho] <akcnl5216@gmail.com>
2024-02-13 14:46:13 +0900

    pri-alb-test

--- test1/sg.tf
+++ test1/sg.tf
@@ -39,6 +39,12 @@ resource "aws_security_group" "web-sg" {
   protocol = "TCP"
   security_groups = [aws_security_group.bastion-sg.id]
   }
+ ingress {
+  from_port = 80
+  to_port = 80
+  protocol = "TCP"
+  cidr_blocks = ["0.0.0.0/0"]
+  }
  egress {
   from_port = 0
   to_port = 0
