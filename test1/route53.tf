3b30947c041bd423d95c31f437655b8dd8c62449
[junho] <akcnl5216@gmail.com>
2024-02-16 09:28:15 +0900

    pri-alb-test

--- test1/route53.tf
+++ test1/route53.tf
@@ -13,6 +13,14 @@ resource "aws_route53_record" "rt53" {
   }
 }
 
+resource "aws_route53_record" "acm" {
+ zone_id = aws_route53_zone.hollyjunho.zone_id
+ name = "_9fb79b2418350ccad1e3b8a7db3f3603.hollyjunho.store."
+ type = "CNAME"
+ ttl = "300"
+ records = ["_a9ed269f4c62812e3023f07bf9745b9e.mhbtsbpdnt.acm-validations.aws."]
+}
+
 output "name_servers" {
  value = aws_route53_zone.hollyjunho.name_servers
 }
