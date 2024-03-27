584bf8e219fa659270c9a15b5363a1577ba3e4d5
[junho] <akcnl5216@gmail.com>
2024-03-27 13:10:07 +0900

    root_module

--- default/main.tf
+++ default/main.tf
@@ -15,3 +15,9 @@ module "vpc" {
   source = "./vpc"
 }
 
+module "ec2" {
+  source = "./ec2"
+  public_subnet_ids = module.vpc.public_subnet_ids
+  private_subnet_ids = module.vpc.private_subnet_ids
+}
+
