c950dd2325efc79ee04d4608674db0774972f862
[junho] <akcnl5216@gmail.com>
2024-04-09 16:33:40 +0900

    module EC2

--- default/ec2/main.tf
+++ default/ec2/main.tf
@@ -4,6 +4,7 @@ resource "aws_instance" "public" {
   subnet_id = var.public_subnet_ids[0]
   associate_public_ip_address = true
   vpc_security_group_ids = [var.security_group_id_bastion]
+  key_name = aws_key_pair.key_pair.key_name
 
   tags = {
     Name = "${var.name}-pub-bastion"
@@ -16,6 +17,7 @@ resource "aws_instance" "private" {
   count = length(var.private_subnet_ids)
   subnet_id = var.private_subnet_ids[count.index]
   vpc_security_group_ids = [var.security_group_id_private]
+  key_name = aws_key_pair.key_pair.key_name
 
   tags = {
     Name = "${var.name}-pri-${count.index+1}"
