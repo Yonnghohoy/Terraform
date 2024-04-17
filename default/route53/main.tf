0b3ce4fea445bafb0f0196219a954c4962a55009
[junho] <akcnl5216@gmail.com>
2024-04-17 14:01:53 +0900

    module R53

--- default/route53/main.tf
+++ default/route53/main.tf
@@ -17,49 +17,6 @@ resource "aws_route53_record" "record" {
 }
 
 ############### Configure ACM ###############
-############### Cloudfront ACM ################
-##provider "aws" {
-##  alias		 = "virginia"
-##  region	 = "us-east-1"
-##}
-#
-#
-################ Create CF ACM # ################
-#resource "aws_acm_certificate" "hollyjunho_cf_acm" {
-#  provider		 = aws.virginia
-#  domain_name		 = "*.hollyjunho.store"
-#  validation_method	 = "DNS"
-#  lifecycle {
-#    create_before_destroy = true
-#  }
-#  tags = {
-#    Name = format("%s-cf-acm", var.name)
-#  }
-#}
-#
-############### attach CF ACM CNAME #############
-#resource "aws_route53_record" "cf-cert_record" {
-#  for_each = {
-#    for dvo in aws_acm_certificate.hollyjunho_cf_acm.domain_validation_options : dvo.domain_name => {
-#      name	= dvo.resource_record_name
-#      record	= dvo.resource_record_value
-#      type	= dvo.resource_record_type
-#    }
-#  }
-#  name			 = each.value.name
-#  allow_overwrite	 = true
-#  records		 = [each.value.record]
-#  ttl			 = 60
-#  type			 = each.value.type
-#  zone_id		 = aws_route53_zone.zone.zone_id
-#}
-#
-#resource "aws_acm_certificate_validation" "cf_acm_validation" {
-#  certificate_arn		= aws_acm_certificate.hollyjunho_cf_acm.arn
-#  validation_record_fqdns	= [for record in aws_route53_record.cf_cert_record : record.fqdn]
-#}
-#
-
 ######## Seoul (ap-northeast-2) ACM ########
 resource "aws_acm_certificate" "hollyjunho_acm" {
   domain_name           = "*.hollyjunho.store"
@@ -67,7 +24,7 @@ resource "aws_acm_certificate" "hollyjunho_acm" {
   lifecycle {
     create_before_destroy = true
   }
-  subject_alternative_names = [ "*.hollyjunho.store" ]
+  subject_alternative_names = ["hollyjunho.store"]
 
   tags = {
     Name = format("%s-cf-acm", var.name)
