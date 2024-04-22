6a0dc24517980e7094a3956702ecc5df43d8d685
[junho] <akcnl5216@gmail.com>
2024-04-22 13:45:19 +0900

    module SG

--- default/sg/main.tf
+++ default/sg/main.tf
@@ -101,3 +101,48 @@ resource "aws_security_group" "web_lb_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+######### RDS SG ############
+resource "aws_security_group" "rds" {
+  name		= format("%s-rds-sg",var.name)
+  description	= "rds security group for ${var.name}"
+  vpc_id	= vpc_id
+
+  egress {
+    from_port 	= 0
+    to_port	= 0
+    protocol	= "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    }
+
+  tags = {
+    Name = format("%s-rds-sg",var.name)
+  }
+}
+
+resource "aws_security_group_rule" "rds_bastion_ssh_ingress" {
+  type				= "ingress"
+  from_port			= "22"
+  to_port			= "22"
+  protocol			= "TCP"
+  security_group_id		= "${aws_security_group.rds.id}"
+  source_security_group_id 	= "${aws_security_group.bastion.id}"
+}
+
+resource "aws_security_group_rule" "rds_bastion_mysql_ingress" {
+  type				= "ingress"
+  from_port			= "3306"
+  to_port			= "3306"
+  protocol			= "TCP"
+  security_group_id		= "${aws_security_group.rds.id}"
+  source_security_group_id 	= "${aws_security_group.bastion.id}"
+}
+
+resource "aws_security_group_rule" "rds_web_mysql_ingress" {
+  type				= "ingress"
+  from_port			= "3306"
+  to_port			= "3306"
+  protocol			= "TCP"
+  security_group_id		= "${aws_security_group.rds.id}"
+  source_security_group_id 	= "${aws_security_group.private.id}"
+}
